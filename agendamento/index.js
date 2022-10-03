46ec53e4fe6cfbaf91e3c61d30215fd02fc322e1
Bruno Venâncio de Souza e Silva <brunosilva.5934@aluno.saojudas>
2022-10-03 15:51:00 -0300

    serviço de agendamento funcionando

--- agendamento/index.js
+++ agendamento/index.js
@@ -1,20 +1,22 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 const servico = express()
-servico.use(bodyParser.json())
+servico.use(express.json())
 const porta = 9000
 
-agendamentos = {}
+const agendamentos = {}
 contador = 0
 
 servico.get("/agendamentos", (req, res) => {
     res.send(agendamentos)
 })
 
-servico.put("/agendamentos", (req, res) => {
-    contador++
-    const { json } = req.body
-    agendamentos[contador] = { contador, json }
+servico.post("/agendamentos", (req, res) => {
+    const { nome } = req.body
+    const { data } = req.body
+    const { hora } = req.body
+    const { local } = req.body
+    agendamentos[++contador] = { contador, nome, data, hora, local }
     res.status(201).send(agendamentos[contador])
 })
 
