bb70f198e8de3e36a9b3d75ceef6f7962ecefd1b
Bruno Venâncio de Souza e Silva <brunosilva.5934@aluno.saojudas.br>
2022-10-09 14:16:16 -0300

    Primeiro teste com base de dados

--- agendamento/index.js
+++ agendamento/index.js
@@ -1,14 +1,26 @@
 import express from 'express'
+import mysql from 'mysql2/promise'
 import Agendamento from '../models/agendamento.mjs'
+
 const servico = express()
 servico.use(express.json())
 const porta = 9000
+const credentials = {
+    host: 'localhost',
+    user: 'projetoA3',
+    password: 'projetoA3',
+    database: 'projetoA3'
+}
 
+let base
 const agendamentos = {}
 let contador = 0
 
-servico.get("/agendamentos", (req, res) => {
-    res.send(agendamentos)
+servico.get("/agendamentos", async (req, res) => {
+    const conn = await mysql.createConnection(credentials)
+    let [rows] = await conn.query("SELECT * FROM Agendamento", [], (err, results, fields) => {})
+    console.log(rows)
+    res.send(base = rows)
 })
 
 servico.post("/agendamentos", (req, res) => {
