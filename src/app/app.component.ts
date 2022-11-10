fdbaada176b25d524207c01288bf8a36501733ee
Bruno Venâncio de Souza e Silva <brunosilva.5934@aluno.saojudas.br>
2022-11-10 17:33:24 -0300

    Construindo a página de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+    title = 'frontend';
+
+    // visible: boolean = false;
+
+    // loginBox() {
+    //   this.visible = this.visible ? false:true;
+    // }
+
+    login!: string;
+    senha!: string;
+
+    enter(user: string, senha: string): void {
+
+    }
 }
